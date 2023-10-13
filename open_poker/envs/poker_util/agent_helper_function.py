b7872d49b5bea5eecd51b974e2cbb4a0cec970ae
minhsueh <minhsueh@usc.edu>
2023-10-13 15:38:36 -0700

    save before modify card_utility_actions

--- open_poker/envs/poker_util/agent_helper_function.py
+++ open_poker/envs/poker_util/agent_helper_function.py
@@ -1,221 +1,218 @@
 import copy
-from itertools import groupby
-from operator import itemgetter
+import collections
+from card_utility_actions import *
 
 
-def get_hand_type(values, suits):
-    """
-    This functions help agent to check whether their current hand is strong or weak
+def format_float_precision(val, significant_digit = 8):
+    """ 
+    Args:
+        val(int|float)
+        significant_digit
 
-    Rank of hand below in order (first one the weakest), extra rule for equal hand is that whose hand has the highest hand win:
-    1. single
-    2. one pair
-    3. two pairs
-    4. three of a kind
-    5. straight
-    6. flush (five of same suit)
-    7. full house (3 + 2)
-    8. four of a kind
-    9. straight flush
-    10. royal flush
-    :param values: list of numbers of current hand
-    :param suits: list of suits of current hand
-    :return:
+    Returns:
+        float
     """
 
-    if is_royal_flush(suits, values):
-        rank_type = 10
-
-    elif is_straight_flush(suits, values):
-        rank_type = 9
-
-    elif is_four_of_a_kind(suits, values):
-        rank_type = 8
-
-    elif is_full_house(suits, values):
-        rank_type = 7
-
-    elif is_flush(suits, values):
-        rank_type = 6
-
-    elif is_straight(suits, values):
-        rank_type = 5
-
-    elif is_three_of_a_kind(suits, values):
-        rank_type = 4
-
-    elif is_two_pair(suits, values):
-        rank_type = 3
-
-    elif is_one_pair(suits, values):
-        rank_type = 2
-
-    else:  # this one is only high card
-        rank_type = 1
-
-    return rank_type
-
-
-def is_one_pair(suits, values):
-    """
-    if hand contains one pairs, return True. Otherwise, return False
-    :param suits: list of current suits
-    :param values: list of current values
-    :return: True if have one pair, otherwise False
-    """
-    for num in set(values):
-        if values.count(num) >= 2:
-            return True
-    return False
-
-
-def is_two_pair(suits, values):
-    """
-    if hand contains two pairs, return True. Otherwise, return False
-    :param suits:
-    :param values:
-    :return:
-    """
-    cnt = 0
-    for num in set(values):
-        if values.count(num) >= 2:
-            cnt += 1
-    return cnt >= 2
-
-
-def is_three_of_a_kind(suits, values):
-    """
-    if hand contains three same number card, return True. Otherwise, return False
-    :param suits:
-    :param values:
-    :return:
-    """
-    for num in set(values):
-        if values.count(num) >= 3:
-            return True
-    return False
-
-
-def is_straight(suits, values):
-    """
-    if hand contains straight e.x 23456, return True. Otherwise, return False
-    :param suits:
-    :param values:
-    :return:
-    """
-    copy_values = copy.copy(values)
-    if 1 in copy_values:
-        copy_values.append(14)
-    copy_values.sort()
-    cnt = 1
-    for i in range(len(copy_values) - 1):
-        if copy_values[i] == copy_values[i+1] - 1:
-            cnt += 1
-        elif cnt >= 5:
-            return True
-        else:
-            cnt = 1
-    return cnt >= 5
-
-
-def is_flush(suits, values):
-    """
-    if hand contains 5 same suit, return True. Otherwise, return False
-    :param suits:
-    :param values:
-    :return:
-    """
-    for suit in set(suits):
-        if suits.count(suit) >= 5:
-            return True
-    return False
-
-
-def is_full_house(suits, values):
-    """
-    if hand contains 3 same number cards and another 2 same number cards, return True. Otherwise, return False
-    :param suits:
-    :param values:
-    :return:
-    """
-    three = 0
-    pair = 0
-    for num in set(values):
-        if values.count(num) == 2:
-            pair += 1
-        elif values.count(num) >= 3:
-            three += 1
-    return (three >= 1 and pair >= 1) or (three >= 2)
-
-
-def is_four_of_a_kind(suits, values):
-    """
-    if hand contains 4 same number cards, return True. Otherwise, return False
-    :param suits:
-    :param values:
-    :return:
-    """
-    for num in set(values):
-        if values.count(num) >= 4:
-            return True
-    return False
-
-
-def is_straight_flush(suits, values):
-    """
-    if hand has flush and straight at same time, return True. Otherwise, return False
-    'club',  'diamond', 'heart', 'spade'
-    :param suits:
-    :param values:
-    :return:
-    """
-    # cards = [(c.get_number(), c.get_suit()) for c in total_hand]
-    cards = [(val, s) for val, s in zip(values, suits)]
-    hand = []
-    if suits.count('club') >= 5:
-        for num, suit in cards:
-            if suit == 'club':
-                hand.append(num)
-    elif suits.count('diamond') >= 5:
-        for num, suit in cards:
-            if suit == 'diamond':
-                hand.append(num)
-    elif suits.count('heart') >= 5:
-        for num, suit in cards:
-            if suit == 'heart':
-                hand.append(num)
-    elif suits.count('spade') >= 5:
-        for num, suit in cards:
-            if suit == 'spade':
-                hand.append(num)
+    if round(float(val), significant_digit) < 0:
+        return(round(0, significant_digit))
+    elif round(float(val), significant_digit) > 1:
+        return(round(1, significant_digit))
     else:
-        return False
-
-    if 1 in hand:
-        hand.append(14)
-
-    hand.sort(key=lambda x: -x)
-    for key, group in groupby(enumerate(hand), key=lambda x: x[0]-x[1]):
-        candidate = list(map(itemgetter(1), group))
-        if len(candidate) >= 5:
-            return True
-
-    return False
+        return(round(float(val), significant_digit))
 
 
-def is_royal_flush(suits, values):
+def get_out_probability(current_gameboard, total_hand, suits, values):
+    """ 
+    
+    current_gameboard['deck'] 
+    current_gameboard['hand_rank_funcs'] is the list where each element contains function is_hand_type, function get_hand_type, hand_type_name(str)
+
+    process:
+    out_probability = dict() # key: hand_type(str), value: (float) probability to get specific hand after next card 
+    iterate hand_type in current_gameboard['hand_rank_funcs']:
+        count_hand = 0
+        total_left_card = 0
+        iterate out_card in whole deck:
+            if out_card not in total_hand:
+                total_left_card += 1
+                if (total_hand + out_card) is hand_type:
+                    count_hand += 1
+        out_probability[hand_type] = round(count_hand/total_left_card, 2)
+
+
+    Args:
+        current_gameboard
+        total_hand
+        suits
+        values
+
+    Returns:
+        (dictionary): 
     """
-    if hand is straight flush and number cards is 10, 11, 12, 13, and A, return True. Otherwise, return False
-    :param suits:
-    :param values:
-    :return:
+    deck = current_gameboard['deck']
+    hand_rank_funcs = current_gameboard['hand_rank_funcs']
+
+    out_probability = dict()
+
+    for check_function, get_function, hand_name in hand_rank_funcs:
+        count_hand = 0
+        total_left_card = 0
+        for out_card in deck:
+            total_left_card += 1
+            if out_card not in total_hand:
+                next_total_hand = total_hand + out_card
+                
+
+
+
+
+def get_probability_royal_flush():
+    pass
+
+def get_probability_straight_flush():
+    pass
+
+def get_probability_four_of_a_kind(total_hand, suits, values):
+    """ 
+    Just consider player's hole card and community card
+
+    Args:
+        current_gameboard
+
+    Returns:
+
     """
-    royal_num = [10, 11, 12, 13, 1]
-    cnt = 0
-    if is_straight_flush(suits, values):
-        for num in royal_num:
-            if num in values:
-                cnt += 1
-    return cnt == 5
+    # assert len(suits) == len(values)
+    hand_count = len(values) # flop: count_dif = 5, turn: count_dif = 6, river:count_dif = 7
+    other_card_count = 52 - hand_count # we don't consider other players' hand becasue we don't have any info
+    assert 5 <= hand_count <=7
+    value_counter = collections.Counter(values)
+    max_count = max(value_counter.values())
+    if max_count == 1:
+        # early stop
+        return(format_float_precision(0))
+
+    if hand_count == 5:
+        # flop
+        if max_count == 2:
+            # have pair
+            if len(value_counter) == 3:
+                # two pairs
+                return(format_float_precision(2/other_card_count/(other_card_count-1)))
+            elif len(value_counter) == 4:
+                # one pair
+                return(format_float_precision(1/other_card_count/(other_card_count-1)))
+
+        elif max_count == 3:
+            # have three of a kind
+            return(format_float_precision(1/other_card_count))
+
+        elif max_count == 4:
+            return(format_float_precision(1))
+
+        else:
+            raise
+
+
+    elif hand_count == 6:
+        # turn
+        if max_count == 2:
+            # have pair
+            return(format_float_precision(0))
+
+        elif max_count == 3:
+            # have three of a kind
+            return(format_float_precision(1/other_card_count))
+
+        elif max_count == 4:
+            return(format_float_precision(1))
+
+        else:
+            raise
+    elif hand_count == 7:
+        # river
+        if max_count == 4:
+            return(format_float_precision(1))
+        else:
+            return(format_float_precision(0))
+    else:
+        raise
+
+
+
+
+
+def get_probability_full_house():
+    hand_count = len(values) # flop: count_dif = 5, turn: count_dif = 6, river:count_dif = 7
+    other_card_count = 52 - hand_count # we don't consider other players' hand becasue we don't have any info
+    assert 5 <= hand_count <=7
+    value_counter = collections.Counter(values)
+    max_count = max(value_counter.values())
+    if max_count == 1:
+        # early stop
+        return(format_float_precision(0))
+
+    if hand_count == 5:
+        # flop
+        if max_count == 2:
+            # have pair
+            if len(value_counter) == 3:
+                # two pairs
+                return(format_float_precision(2/other_card_count/(other_card_count-1)))
+            elif len(value_counter) == 4:
+                # one pair
+                return(format_float_precision(1/other_card_count/(other_card_count-1)))
+
+        elif max_count == 3:
+            # have three of a kind
+            return(format_float_precision(1/other_card_count))
+
+        elif max_count == 4:
+            return(format_float_precision(1))
+
+        else:
+            raise
+
+
+    elif hand_count == 6:
+        # turn
+        if max_count == 2:
+            # have pair
+            return(format_float_precision(0))
+
+        elif max_count == 3:
+            # have three of a kind
+            return(format_float_precision(1/other_card_count))
+
+        elif max_count == 4:
+            return(format_float_precision(1))
+
+        else:
+            raise
+    elif hand_count == 7:
+        # river
+        if max_count == 4:
+            return(format_float_precision(1))
+        else:
+            return(format_float_precision(0))
+    else:
+        raise
+
+def get_probability_flush():
+    pass
+
+def get_probability_straight():
+    pass
+
+def get_probability_three_of_a_kind():
+    pass
+
+def get_probability_two_pair():
+    pass
+
+def get_probability_one_pair():
+    pass
+
+
 
 
--- test/test_np.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-np.random.seed(5)
-first_x_card = 5
-a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-first_portion = a[:first_x_card]
-second_portion = a[first_x_card:]
-np.random.shuffle(first_portion)
-a = first_portion + second_portion
-print(a)
