6e024f04340fbd450f85bd56ec0a7ba5c4b28dce
minhsueh <minhsueh@usc.edu>
2023-12-26 20:04:05 -0800

    modify pacakge name

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name="gym-open-poker",
-    packages = ["gym-open-poker"],
-    description='gym-open-poker',
-    version="0.0.1",
+    name="gym_open_poker",
+    packages = ["gym_open_poker"],
+    description='gym_open_poker',
+    version="0.0.3",
     author='Min-Hsueh Chiu',
     author_email='peter810601@gmail.com',
     url="https://github.com/minhsueh/gym-open-poker",
     install_requires=["gym==0.26.0", "pygame==2.1.0"],
-    python_requires="==3.9"
+    python_requires=">=3.9"
 )
