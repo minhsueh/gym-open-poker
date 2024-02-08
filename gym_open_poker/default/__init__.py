a8139d53b8a9bc66d4b9143a77310d26ec20ee5e
minhsueh <minhsueh@usc.edu>
2024-02-08 14:14:05 -0800

    update testing file path

--- gym_open_poker/default/__init__.py
+++ gym_open_poker/default/__init__.py
@@ -1,6 +1,8 @@
 import yaml
+import os 
 
-config_path = './config.yaml'
+current_path = os.path.dirname(os.path.realpath(__file__))
+config_path = current_path + '/config.yaml'
 with open(config_path, "r") as stream:
     try:
         default_config_dict = yaml.safe_load(stream)
