b4b0ee0bdfd376d8330db7e153db3b50644e08eb
minhsueh <minhsueh@usc.edu>
2024-03-10 22:16:29 -0700

    update pytest

--- tests/test.sh
+++ tests/test.sh
@@ -9,7 +9,7 @@ IFS=$'\n'  # Set Internal Field Separator to newline to handle file names with s
 for file in $test_files; do
     echo "Processing file: $file"
 
-    pytest -s $file
+    pytest $file
     # Exit with a non-zero status if pytest encounters failures
     if [ $? -ne 0 ]; then
         exit 1
